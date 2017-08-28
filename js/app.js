959f3aaf6d32ef2b02374be67d145143c46ac81a
tbarf <30032737+tbarf@users.noreply.github.com>
2017-08-28 00:55:08 -1000

    done

--- js/app.js
+++ js/app.js
@@ -11,31 +11,56 @@ Please also use CSS to style up your work
 */
 
 
-var kingdoms = [{ crest: "https://vignette4.wikia.nocookie.net/gameofthrones/images/1/15/House-Arryn-Main-Shield.PNG/revision/latest?cb=20170101094153", house: "House Arryn",region: "The Vale of Arryn",lord: "Robin Arryn", allegiance: "House Stark"},{ crest: "https://vignette1.wikia.nocookie.net/gameofthrones/images/e/e6/House-Frey-Main-Shield.PNG/revision/latest?cb=20170523011255", house: "House Frey",region: "The Riverlands",lord: "Unknown", allegiance: "House Lanister"},{ crest: "https://vignette3.wikia.nocookie.net/gameofthrones/images/8/86/House-Greyjoy-Main-Shield.PNG/revision/latest?cb=20170523015836", house: "House Greyjoy",region: "Iron Islands",lord: "King Euron Greyjoy", allegiance: "Sovereign"},{ crest: "http://awoiaf.westeros.org/images/thumb/d/d5/House_Lannister.svg/250px-House_Lannister.svg.png", house: "House Lannister",region: "The Crownlands",lord: "Queen Cersei", allegiance: "House Targaryen"},{ crest: "http://vignette4.wikia.nocookie.net/gameofthrones/images/8/8a/House-Stark-Main-Shield.PNG/revision/latest?cb=20170101103142", house: "House Stark",region: "The North",lord: "King Jon Snow", allegiance: "Sovereign"},{ crest: "https://vignette2.wikia.nocookie.net/gameofthrones/images/4/43/House-Targaryen-Main-Shield.PNG/revision/latest?cb=20170510235320", house: "House Targaryen",region: "Dragonstone",lord: "Queen Daenerys", allegiance: "Sovereign"}];
+var kingdoms = [{ crest: "https://vignette4.wikia.nocookie.net/gameofthrones/images/1/15/House-Arryn-Main-Shield.PNG/revision/latest?cb=20170101094153", house: "House Arryn", saying:"As High as Honor",region: "The Vale of Arryn",lord: "Robin Arryn", allegiance: "House Stark"},{ crest: "https://vignette1.wikia.nocookie.net/gameofthrones/images/e/e6/House-Frey-Main-Shield.PNG/revision/latest?cb=20170523011255", house: "House Frey", saying:"We Stand Together",region: "The Riverlands",lord: "Unknown", allegiance: "House Lanister"},{ crest: "https://vignette3.wikia.nocookie.net/gameofthrones/images/8/86/House-Greyjoy-Main-Shield.PNG/revision/latest?cb=20170523015836", house: "House Greyjoy", saying:" We Do Not Sow",region: "Iron Islands",lord: "King Euron Greyjoy", allegiance: "Sovereign"},{ crest: "http://awoiaf.westeros.org/images/thumb/d/d5/House_Lannister.svg/250px-House_Lannister.svg.png", house: "House Lannister", saying:"A Lannister always pays her debts",region: "The Crownlands",lord: "Queen Cersei", allegiance: "House Targaryen"},{ crest: "http://vignette4.wikia.nocookie.net/gameofthrones/images/8/8a/House-Stark-Main-Shield.PNG/revision/latest?cb=20170101103142", house: "House Stark", saying:" Winter is Coming",region: "The North",lord: "King Jon Snow", allegiance: "Sovereign"},{ crest: "https://vignette2.wikia.nocookie.net/gameofthrones/images/4/43/House-Targaryen-Main-Shield.PNG/revision/latest?cb=20170510235320", house: "House Targaryen", saying:"Fire and Blood",region: "Dragonstone",lord: "Queen Daenerys", allegiance: "Sovereign"}];
 
 
 var main = document.body;
 
+var mainDiv = document.createElement('div');
+mainDiv.className = "l-wrap";
+main.appendChild(mainDiv);
+
 for(var i =0; i<kingdoms.length; i++){
-	var houseBox = document.createElement('div');
-	houseBox.id = "house";
-  houseBox.innerHTML = "Kingdom: " + kingdoms[i].house;
-	main.appendChild(houseBox);
-	var regionBox = document.createElement('div');
-	regionBox.id = "region";
-  regionBox.innerHTML = "Region: " + kingdoms[i].region;
-	main.appendChild(regionBox);
-	var lordBox = document.createElement('div');
-  lordBox.id = "lord";
-	lordBox.innerHTML = "Lord: " + kingdoms[i].lord;
-	main.appendChild(lordBox);
-	var allegianceBox = document.createElement('div');
-  allegianceBox.id = "allegiance";
-	allegianceBox.innerHTML ="Allegiance: " + kingdoms[i].allegiance;
-	main.appendChild(allegianceBox);
-	var crestBox = document.createElement('img');
-	crestBox.id = "crest";
+var ChildDiv = document.createElement('div');
+ChildDiv.className = "three-col-grid" + i;
+mainDiv.appendChild(ChildDiv);
+
+var houseBox = document.createElement('div');
+	houseBox.className = "grid-item";
+	houseBox.id = "house" + i;
+  houseBox.textContent = "Kingdom: " + kingdoms[i].house;
+	ChildDiv.appendChild(houseBox);
+
+
+var sayingBox = document.createElement('div');
+sayingBox.className = "grid-item";
+sayingBox.id = "saying" + i;
+sayingBox.textContent = "Saying: " + kingdoms[i].saying;
+ChildDiv.appendChild(sayingBox);
+
+
+
+var crestBox = document.createElement('img');
+	crestBox.id = "crest" + i;
+	crestBox.className = "image";
 	crestBox.src = kingdoms[i].crest;
-  main.appendChild(crestBox);
+  	ChildDiv.appendChild(crestBox);
+
+
+	var regionBox = document.createElement('div');
+	regionBox.id = "region" + i;
+	regionBox.className = "grid-item";
+  regionBox.textContent = "Region: " + kingdoms[i].region;
+	ChildDiv.appendChild(regionBox);
+	var lordBox = document.createElement('div');
+  lordBox.id = "lord" + i;
+  lordBox.className = "grid-item";
+	lordBox.textContent = "Lord: " + kingdoms[i].lord;
+	ChildDiv.appendChild(lordBox);
+	var allegianceBox = document.createElement('div');
+  allegianceBox.id = "allegiance" + i;
+  allegianceBox.className = "grid-item";
+	allegianceBox.textContent ="Allegiance: " + kingdoms[i].allegiance;
+	ChildDiv.appendChild(allegianceBox);
+
 }
-console.log("hi");
